1e831c57ae0feece31178d6a6b0f98973b32df96
Nakorn K <nakorn.kitk@gmail.com>
2024-12-20 00:40:31 +0700

    start changing from using mongoodb to mysql

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors =  require("cors")
-const mongoose =  require("mongoose")
+// const mongoose =  require("mongoose")
 require("dotenv").config()
 const blogRoute = require('./routes/blog')
 const authRoute = require('./routes/auth')
