4f60def8518093da59d403de4da28e6f93854b9f
Nakorn K <nakorn.kitk@gmail.com>
2024-12-26 02:51:37 +0700

    commit adding client file

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ var cors =  require('cors')
 var app = express()
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+var jwt = require('jsonwebtoken')
+require('dotenv').config()
 
 app.use(cors())
 
@@ -11,23 +15,56 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'my_webboard'
+    database: 'my_webboard',
+    port: 3307
 });
 
 
 app.post('/register', jsonParser, function (req, res, next) {
-    connection.execute(
-        'INSERT INTO users (email, password, fname, lname) VALUES (?,?,?,?)',
-        [req.body.email, req.body.password, req.body.fname, req.body.lname],
-        function (err, results, fields) {
-            if (err) {
-                res.json({ status: 'error', message: err });
-                return; // Ensure no further execution
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+        connection.execute(
+            'INSERT INTO users (email, password, fname, lname) VALUES (?,?,?,?)',
+            [req.body.email, hash, req.body.fname, req.body.lname],
+            function (err, results, fields) {
+                if (err) {
+                    res.json({ status: 'error', message: err });
+                    return; // Ensure no further execution
+                }
+                res.json({ status: 'ok' });
             }
-            res.json({ status: 'ok' });
+        );
+    })
+});
+
+app.post('/login', jsonParser, function(req,res,next){
+    connection.execute(
+        'SELECT * FROM users WHERE email=?',
+        [req.body.email],
+        function (err, users, fields) {
+            if (err) {res.json({ status: 'error', message: err }); return}
+            if (users.length == 0) {res.json({ status: 'error', message: 'no user found' }); return}
+            bcrypt.compare(req.body.password, users[0].password, function(err, isLogin){
+                if (isLogin) {
+                    var token = jwt.sign({email: users[0].email}, process.env.SECRET_KEY, { expiresIn: '1h' }  )
+                    res.json({status: 'ok', message: 'login success', token})
+                } else {
+                    res.json({status: 'error', message: 'login failed'})
+                }
+            });
         }
     );
-});
+})
+
+
+app.post('/authen', jsonParser, function(req,res,next){
+    try {
+        const token = req.headers.authorization.split(' ')[1]
+        var decoded = jwt.verify(token, process.env.SECRET_KEY)
+        res.json({status: 'ok', decoded})
+    } catch(err) {
+        res.json({status: 'error', message: err.message})
+    }
+})
 
 
 app.listen(8080, function(){
